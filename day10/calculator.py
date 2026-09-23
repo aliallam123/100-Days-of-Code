7148abc72faaabf0fd7d4060ce5eeacea81c8ecf
aliallam123 <bantalibt@gmail.com>
2026-09-23 19:03:39 +0100

    feat(day10): add basic arithmetic functions

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,3 +1,11 @@
 ﻿# In Python, multi-line comments are created using '#' at the start of each line.
 # Shortcut: Press Ctrl + / (or Cmd + / on Mac) in your IDE to comment/uncomment multiple lines.
 # Note: Triple quotes (''' or """) create multi-line strings / docstrings rather than true comments.
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
