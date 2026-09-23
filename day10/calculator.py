d00686d4efd677c634c00a1a2086e197c460b58e
aliallam123 <bantalibt@gmail.com>
2026-09-23 19:03:14 +0100

    chore(day10): add calculator notes and setup file

---
+++ day10/calculator.py
@@ -0,0 +1,3 @@
+﻿# In Python, multi-line comments are created using '#' at the start of each line.
+# Shortcut: Press Ctrl + / (or Cmd + / on Mac) in your IDE to comment/uncomment multiple lines.
+# Note: Triple quotes (''' or """) create multi-line strings / docstrings rather than true comments.
